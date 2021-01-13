f8308a19ba2e5fc7f8a86df27e8edf3f0d954795
yueming xu <xnysym@outlook.com>
2021-01-13 19:29:06 +0800

    init

---
+++ Moon.ComputerComposition/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Threading;
+
+namespace Moon.ComputerComposition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Interlocked.Add(ref i, 1);
+            
+            ThreadPool.QueueUserWorkItem(state =>
+            {
+                Console.WriteLine("123");
+
+            });
+        }
+
+        public void Write()
+        {
+            Console.WriteLine("123");
+        }
+    }
+}
